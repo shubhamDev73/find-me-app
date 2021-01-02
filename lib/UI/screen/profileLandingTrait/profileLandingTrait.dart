9a39e7f20c3d20368e741eed1b6e3822a4048602
Sudhanshu Pandey <pandey.rdr1999@gmail.com>
2021-01-02 19:27:30 +0530

    Updated carousel slider for adjectives
    There night be issues with positioning - needs to be reviewed on multiple devices.
    
    Text formating for one of the adj. is not proper - issue might be with the data on backend

--- lib/UI/screen/profileLandingTrait/profileLandingTrait.dart
+++ lib/UI/screen/profileLandingTrait/profileLandingTrait.dart
@@ -69,6 +69,7 @@ class _ProfileLandingTraitState extends State<ProfileLandingTrait> {
   String trait = '';
   double value = 0.50;
   List adjectives = [];
+  int _current = 0;
 
   Future<Map<String, dynamic>> futurePersonality;
 
@@ -83,25 +84,109 @@ class _ProfileLandingTraitState extends State<ProfileLandingTrait> {
     });
   }
 
-  CarouselSlider buildAdjCarouselSlider() {
-    return CarouselSlider(
-      options: CarouselOptions(
-        // height: height,
-        viewportFraction: 1.0,
-        enlargeCenterPage: false,
-        initialPage: 0,
-        aspectRatio: 2.0,
+  Container buildAdjCarouselSlider() {
+    return Container(
+      constraints: BoxConstraints(
+          minWidth: 340, minHeight: 206, maxWidth: 340, maxHeight: 206),
+      decoration: BoxDecoration(
+          color: const Color(0xFFE0F7FA),
+          borderRadius: BorderRadius.circular(15)),
+      margin: EdgeInsets.fromLTRB(
+        10.0,
+        0.0,
+        10.0,
+        0.0,
       ),
-      items: adjectives
-          .map((adjective) => Builder(
-                builder: (BuildContext context) {
-                  return AdjListItem(
-                      adj: adjective['name'],
-                      des: "OHHHHHH BOYYYYAAAAA",
-                      des2: adjective['description']);
-                },
-              ))
-          .toList(),
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Positioned(
+            child: CarouselSlider(
+              options: CarouselOptions(
+                  // height: height,
+                  viewportFraction: 1.0,
+                  enlargeCenterPage: false,
+                  initialPage: 0,
+                  aspectRatio: 2.0,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  }),
+              items: adjectives
+                  .map((adjective) => Builder(
+                        builder: (BuildContext context) {
+                          return AdjListItem(
+                            adj: adjective['name'],
+                            des: "OHHHHHH BOYYYYAAAAA",
+                            des2:
+                                "this trait of ether shows lorem epsum about you this space will provide information about each facet of the personality in detail",
+                            // des2: adjective['description']
+                          );
+                        },
+                      ))
+                  .toList(),
+            ),
+          ),
+          Positioned(
+            top: 125,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: adjectives.map((url) {
+                int index = adjectives.indexOf(url);
+                return Container(
+                  width: 8.0,
+                  height: 8.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: _current == index
+                        ? null
+                        : Border.all(color: Colors.black),
+                    color: _current == index
+                        ? Color(0xFF000000)
+                        : Color(0xFFFFFFFF),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              constraints: BoxConstraints(
+                  minWidth: 340, minHeight: 45, maxWidth: 340, maxHeight: 45),
+              decoration: BoxDecoration(
+                  // color: const Color(0xFFE0F7FA),
+                  color: const Color(0xFFE0F7FA),
+                  borderRadius: BorderRadius.circular(15)),
+              child: Center(
+                child: Container(
+                  constraints: BoxConstraints(
+                      minHeight: 31,
+                      minWidth: 101,
+                      maxWidth: 101,
+                      maxHeight: 31),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Explore",
+                      style: TextStyle(
+                          fontFamily: 'Quicksand',
+                          fontSize: 12,
+                          color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
