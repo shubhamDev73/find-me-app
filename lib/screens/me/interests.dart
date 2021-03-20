6e885dd655e31b88b1539407e0737ec3697bd1cd
Shubham Gupta <shubham0209@gmail.com>
2021-03-21 01:34:37 +0530

    Made seperate interest questions widget and added dots for list

--- lib/screens/me/interests.dart
+++ lib/screens/me/interests.dart
@@ -11,31 +11,172 @@ import 'package:findme/models/user.dart';
 import 'package:findme/API.dart';
 import 'package:findme/globals.dart' as globals;
 
-Widget createQuestions(User user, Function onPageChange, int interestId, CarouselController controller) {
-  Interest interest = user.interests[interestId];
-  return CarouselSlider(
-    carouselController: controller,
-    items: interest.questions.map((question) => Container(
-      margin: EdgeInsets.symmetric(horizontal: 35),
-      child: Center(
-        child: Text(
-          question['question'],
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.w700,
+class QuestionsWidget extends StatefulWidget {
+
+  final List<dynamic> questions;
+  final bool me;
+  final int interestId;
+  QuestionsWidget({this.me, this.interestId, this.questions});
+
+  @override
+  _QuestionsWidgetState createState() => _QuestionsWidgetState();
+}
+
+class _QuestionsWidgetState extends State<QuestionsWidget> {
+
+  CarouselController buttonCarouselController = CarouselController();
+  Map<String, dynamic> currentQuestion;
+
+  @override
+  void initState() {
+    super.initState();
+    currentQuestion = widget.questions[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(!widget.questions.contains(currentQuestion)) currentQuestion = widget.questions[0];
+    TextEditingController answerController = TextEditingController(text: currentQuestion['answer']);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          flex: 4,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              CarouselSlider(
+                carouselController: buttonCarouselController,
+                items: widget.questions.map((question) => Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: Center(
+                    child: Text(
+                      question['question'],
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                )).toList(),
+                options: CarouselOptions(
+                  initialPage: 0,
+                  enlargeCenterPage: true,
+                  autoPlay: false,
+                  scrollDirection: Axis.horizontal,
+                  enableInfiniteScroll: true,
+                  aspectRatio: 2.0,
+                  onPageChanged: (index, reason) => setState(() {
+                    currentQuestion = widget.questions[index];
+                  }),
+                ),
+              ),
+              Positioned(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12.0),
+                      child: InkWell(
+                        onTap: () => buttonCarouselController.previousPage(
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.decelerate,
+                        ),
+                        child: Container(
+                          child: Center(
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.grey.shade800,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 12.0),
+                      child: InkWell(
+                        onTap: () => buttonCarouselController.nextPage(
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.decelerate,
+                        ),
+                        child: Container(
+                          child: Center(
+                            child: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.grey.shade800,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-      ),
-    )).toList(),
-    options: CarouselOptions(
-        initialPage: 0,
-        enlargeCenterPage: true,
-        aspectRatio: 2.0,
-        onPageChanged: (index, reason) {
-          onPageChange(interest.questions[index]);
-        }),
-  );
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 80, vertical: 0),
+          child: widget.me ? TextField(
+            controller: answerController,
+            onSubmitted: (text) => updateAnswer(widget.interestId, currentQuestion['id'], text),
+            textAlign: TextAlign.center,
+            style: GoogleFonts.quicksand(
+              textStyle: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ) :
+          Text(
+            currentQuestion['answer'],
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: widget.questions.map((question) {
+              return Container(
+                width: 8.0,
+                height: 8.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: question['id'] == currentQuestion['id'] ? null : Border.all(color: Colors.black),
+                  color: question['id'] == currentQuestion['id'] ? Colors.black : Colors.white,
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void updateAnswer(int interestId, int questionId, String answer) {
+    POST('me/interests/$interestId/update/', jsonEncode([{"question": questionId, "answer": answer}]));
+
+    globals.meUser.update((User user) {
+      Interest interest = user.interests[interestId];
+      for (int i = 0; i < interest.questions.length; i++) {
+        if (interest.questions[i]['id'] == questionId) {
+          interest.questions[i]['answer'] = answer;
+          break;
+        }
+      }
+      user.interests[interestId] = interest;
+      return user;
+    });
+  }
+
 }
 
 List<Widget> getInterestList(List<Interest> obj, Function onClick, int id) {
@@ -43,14 +184,14 @@ List<Widget> getInterestList(List<Interest> obj, Function onClick, int id) {
   List<Widget> widgetList = [];
   int itemCounter = 0;
 
-  for (int i = 0; i <= (obj.length ~/ 5) * 2; i++) {
+  for(int i = 0; i <= (obj.length ~/ 5) * 2; i++){
     List<Widget> tempRow = [];
-    for (int j = 0; j < i % 2 + 2; j++) {
+    for(int j = 0; j < i % 2 + 2; j++){
       if(itemCounter >= obj.length) break;
-      dynamic interest = obj[itemCounter++];
+      Interest interest = obj[itemCounter++];
       tempRow.add(InterestButton(
         name: interest.name,
-        onClick: () {onClick(interest.id);},
+        onClick: () => onClick(interest.id),
         amount: interest.amount,
         selected: interest.id == id,
       ));
@@ -80,44 +221,20 @@ class Interests extends StatefulWidget {
 class _InterestsState extends State<Interests> {
 
   int interestId;
-  Map<String, dynamic> question = {"id": 0, "answer": ""};
 
   @override
-  void initState () {
+  void initState() {
     super.initState();
     globals.onInterestsChanged = () {
       setState(() {});
     };
   }
 
-  void updateAnswer(int interestId, int questionId, String answer) {
-    POST('me/interests/$interestId/update/', jsonEncode([{"question": questionId, "answer": answer}]));
-
-    globals.meUser.update((User user) {
-      Interest interest = user.interests[interestId];
-      for (int i = 0; i < interest.questions.length; i++) {
-        if (interest.questions[i]['id'] == questionId) {
-          interest.questions[i]['answer'] = answer;
-          break;
-        }
-      }
-      user.interests[interestId] = interest;
-      return user;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     if (interestId == null) interestId = ModalRoute.of(context).settings.arguments;
 
-    CarouselController buttonCarouselController = CarouselController();
-    TextEditingController answerController = new TextEditingController(text: question['answer']);
-
     return createFutureWidget<User>(globals.getUser(me: widget.me), (User user) {
-      if(question['id'] == 0) {
-        question = user.interests[interestId].questions[0];
-        answerController.text = question['answer'];
-      }
       return Scaffold(
         body: SafeArea(
           child: Column(
@@ -134,102 +251,17 @@ class _InterestsState extends State<Interests> {
               ),
               Expanded(
                 flex: 6,
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    createQuestions(user, (Map<String, dynamic> newQuestion) {
-                      setState(() {
-                        question = newQuestion;
-                      });
-                    }, interestId, buttonCarouselController),
-                    Positioned(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 12.0),
-                            child: InkWell(
-                              onTap: () => buttonCarouselController.previousPage(
-                                  duration: Duration(milliseconds: 300),
-                                  curve: Curves.decelerate),
-                              child: Container(
-                                child: Center(
-                                  child: Icon(
-                                    Icons.arrow_back_ios,
-                                    color: Colors.grey.shade800,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 12.0),
-                            child: InkWell(
-                              onTap: () => buttonCarouselController.nextPage(
-                                  duration: Duration(milliseconds: 300),
-                                  curve: Curves.decelerate),
-                              child: Container(
-                                child: Center(
-                                  child: Icon(
-                                    Icons.arrow_forward_ios,
-                                    color: Colors.grey.shade800,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                flex: 5,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 80, vertical: 0),
-                      child: widget.me ? TextField(
-                        controller: answerController,
-                        onSubmitted: (text) {updateAnswer(interestId, question['id'], text);},
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.quicksand(
-                          textStyle: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ) :
-                      Text(
-                        question['answer'],
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 27),
-                  ],
-                ),
+                child: QuestionsWidget(me: widget.me, interestId: interestId, questions: user.interests[interestId].questions),
               ),
               Expanded(
                 flex: 6,
                 child: Container(
                   padding: const EdgeInsets.fromLTRB(0.0, 17.0, 0.0, 0.0),
                   color: Color(0xfff0fbfd),
-                  child: Scrollbar(
-                      child: ListView(
-                        children: getInterestList(user.interests.values.toList(), (int newInterestId) {
-                          setState(() {
-                            interestId = newInterestId;
-                            question = user.interests[interestId].questions[0];
-                          });
-                        }, interestId),
-                      )
+                  child: ListView(
+                    children: getInterestList(user.interests.values.toList(), (int newInterestId) => setState(() {
+                      interestId = newInterestId;
+                    }), interestId),
                   ),
                 ),
               ),
